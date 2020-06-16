382f41c0c69c74cd53800804aa2a29d897f9508b
Roberto Kendy Sawamura <rkendy@gmail.com>
2020-06-16 18:02:40 -0300

    Parameterized tests

---
+++ src/main/java/br/com/rkendy/Animal.java
@@ -0,0 +1,5 @@
+package br.com.rkendy;
+
+public enum Animal {
+    ELEPHANT, TIGER, DOG, CAT, MOUSE
+}
--- src/test/java/br/com/rkendy/OddEvenTest.java
+++ src/test/java/br/com/rkendy/OddEvenTest.java
@@ -1,8 +1,21 @@
 package br.com.rkendy;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class OddEvenTest {
 
@@ -11,11 +24,64 @@ class OddEvenTest {
 		OddEven oddEven = new OddEven();
 		assertTrue(oddEven.isNumberEven(10));
 	}
-	
+
 	@Test
 	void givenOddNumber_whenIsEvenIsCalled_thenFalseIsReturned() {
 		OddEven oddEven = new OddEven();
 		assertFalse(oddEven.isNumberEven(11));
 	}
 
+	@ParameterizedTest
+	@ValueSource(ints = { 2, 4, 6, 8, Integer.MAX_VALUE })
+	void givenANumber_whenIsEvenIsCalled_thenTrueIsReturnedForEvenNumbers(int number) {
+		OddEven oddEven = new OddEven();
+		assertTrue(oddEven.isNumberEven(number));
+	}
+
+	@ParameterizedTest
+	@EnumSource(Animal.class)
+	void testEnumSource(Animal animal) {
+		assertNotNull(animal);
+	}
+
+	@ParameterizedTest
+	@MethodSource("parameterProvider")
+	void testWithSimpleMethodSource(String argument) {
+		// assertNotNull(argument);
+		assertTrue(argument.startsWith("param"));
+	}
+
+	// method name is the source to test
+	static Stream<String> parameterProvider() {
+		return Stream.of("parameter1", "parameter2");
+	}
+
+	@ParameterizedTest
+	@CsvSource({ "one, 1", "two, 2", "'foo, bar', 3" })
+	void testWithCsvSource(String first, int second) {
+		assertNotNull(first);
+		assertNotEquals(0, second);
+	}
+
+	@ParameterizedTest
+	@CsvSource({ "one, " })
+	void testWithCsvSource2(String first, String second) {
+		assertNotNull(first);
+		assertNull(second);
+	}
+
+	@ParameterizedTest
+	@CsvSource({ "one, 'two,three'" })
+	void testWithCsvSource3(String first, String second) {
+		assertNotNull(first);
+		assertEquals("two,three", second);
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/capitals.csv", numLinesToSkip = 1)
+	void testWithCsvFileSource(String country, String capital) {
+		System.out.println(country + ": " + capital);
+		assertNotNull(country);
+		assertNotNull(capital);
+	}
 }
